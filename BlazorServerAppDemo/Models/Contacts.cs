d33a569038af0d50533527f1a2a0bc6a2ac8851d
Kajal Shukla <kajalshukla124@gmail.com>
2023-05-02 16:12:35 +0530

    Initial commit

---
+++ BlazorServerAppDemo/Models/Contacts.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorServerAppDemo.Models
+{
+    [Serializable]
+    public class Contacts
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }      
+        public string Email { get; set; }
+    }
+}
---
+++ BlazorServerAppDemo/Pages/Index.razor.cs
@@ -0,0 +1,52 @@
+﻿using BlazorServerAppDemo.Models;
+
+namespace BlazorServerAppDemo.Pages
+{
+    public partial class Index
+    {
+        private List<Contacts> contacts;
+        public Dictionary<string, object> MyTextboxAttribute = new Dictionary<string, object>
+        {
+         {"placeholder","Contact Name"},
+         {"type","text" },
+         {"disabled" ,"disabled"}
+
+        };
+
+        private void DeleteContact(Contacts contact)
+        {
+            contacts.Remove(contact);
+        }
+
+        protected async override Task OnInitializedAsync()
+        {
+            await Task.Delay(5000);
+
+            contacts = new List<Contacts>
+        {
+            new Contacts
+            {
+                    FirstName="Kajal",
+                    LastName="Shukla",
+                    Email="kajal@gmail.com"
+            },
+            new Contacts
+            {
+                FirstName="Puru",
+                LastName="Chaudhary",
+                Email="puru@gmail.com"
+            },
+            new Contacts
+            {
+                FirstName="Khushi",
+                LastName="Singh",
+                Email="khushi@gmail.com"
+            }
+        };
+            base.OnInitialized();
+
+        }
+
+       
+    }
+}
